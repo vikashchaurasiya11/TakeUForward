0c82642d370ef02124e148c5c17d4b107642b2d9
vikashchaurasiya11 <vikasch8059@gmail.com>
2026-06-13 18:51:38 +0530

    7pt

---
+++ 2Patterns/7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void pattern7(int n) {
+        for(int i=0;i<n;i++){
+            for(int k=0;k<n-i-1;k++){
+                cout<<" ";
+            }
+            for(int j=0;j<(2*i)+1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
